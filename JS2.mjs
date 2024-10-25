780a3cfe4c850254bd3bab43396cb6e16797e072
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-10-25 10:44:59 -0400

    import export

---
+++ JS2.mjs
@@ -0,0 +1,7 @@
+const message = () => {
+const first = "Hello World";
+let age = "30";
+return first + ". I am " + age + " years old.";
+};
+
+export default message;
---
+++ main.mjs
@@ -0,0 +1,3 @@
+
+import message from "./JS2.mjs"
+console.log(message());
